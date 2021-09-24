e2974813bddc2d34ec6e546a70495a42193305eb
Taite Jernigan <taite5100@gmail.com>
2021-09-23 17:25:48 -0700

    Finished Linked List from part 2

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,40 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    var currentNode = Node(value);
+    if (list.head === null) {
+      list.head = currentNode;
+    } else {
+      list.tail.next = currentNode;
+    }
+    list.tail = currentNode;
+
   };
 
   list.removeHead = function() {
+    var currentHead = list.head;
+    var currentHeadValue = list.head.value;
+    if (JSON.stringify(list.head) !== JSON.stringify(list.tail)) {
+      list.head = currentHead.next;
+    } else {
+      currentHead = null;
+      list.tail = null;
+    }
+    return currentHeadValue;
   };
 
   list.contains = function(target) {
+    var hasTarget = false;
+    debugger;
+    var findTarget = function (target, currentNode) {
+      if (currentNode.value !== target && currentNode.next !== null) {
+        findTarget(target, currentNode.next);
+      } else if (currentNode.value === target) {
+        return hasTarget = true;
+      }
+    };
+    findTarget(target, list.head);
+    return hasTarget;
   };
 
   return list;
